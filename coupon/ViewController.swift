2da5273cb27a8744c97f40bb29884b60af8b2672
naoto-ueda <uettyan@gmail.com>
2021-11-11 22:55:11 +0900

    List表示を実装　内容は適当

--- coupon/ViewController.swift
+++ coupon/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    let TODO = ["牛乳を買う", "掃除をする", "アプリ開発の勉強をする"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
-
-
+    
+    // セルの個数を指定するデリゲートメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    // セルに値を設定するデータソースメソッド（必須）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = TODO[indexPath.row]
+        return cell
+    }
+    
 }
 
