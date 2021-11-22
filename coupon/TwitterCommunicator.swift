54072f3bf0e84d1627d4d15ae3c24137ab212a1d
naoto-ueda <uettyan@gmail.com>
2021-11-23 04:09:31 +0900

    非公開データを別ファイルに外出し

--- coupon/TwitterCommunicator.swift
+++ coupon/TwitterCommunicator.swift
@@ -8,24 +8,42 @@
 import Foundation
 import Social
 
-struct TwitterCommunicator {
-    func getTimeline(handler: @escaping (Data?, Error?) -> ()) {
-        let request = SLRequest(
-            forServiceType: SLServiceTypeTwitter,
-            requestMethod: .GET,
-            url: URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json"),
-            parameters: nil
-        )
+class TwitterCommunicator {
+    let tweetFields : String = "tweet.fields=author_id,id,text,created_at"
+    let maxResults : String = "max_results=100"
+    
+    /// TwitterAPIから同期式でクーポン情報(json)を取得する
+    /// - Returns: json形式のクーポン情報
+    func getCouponFromTwitter(handler: @escaping (Data?, Error?) -> ())
+    {
+        var target : String = "(抽選 OR 応募) -is:retweet ("
+        coupon.PrivateData.COUPON_ACCOUNTS.forEach {
+            target.append("from:")
+            target.append($0)
+            if coupon.PrivateData.COUPON_ACCOUNTS.last != $0 {
+                target.append(" OR ")
+            }
+        }
+        target.append(")")
+
+        let targetEncodeString = target.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
+        let query : String = "query=\(targetEncodeString!)"
         
-        request?.account = Account.twitterAccount
+        // Httpリクエストの生成
+        // TODO URLコンポーネントを使用した形にしたい https://qiita.com/shungo_m/items/64564fd822a7558ac7b1
+        var request = URLRequest(url: URL(string: "https://api.twitter.com/2/tweets/search/recent?\(query)&\(tweetFields)&\(maxResults)")!)
+        request.addValue("Bearer \(coupon.PrivateData.BEARER)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
         
-        request?.perform { data, response, error in
+        // HTTPリクエスト実行
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 handler(nil, error)
                 return
             }
-            
+
             handler(data, error)
         }
+        task.resume()
     }
 }
